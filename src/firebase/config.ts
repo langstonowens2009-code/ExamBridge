7312d4eda3479b3e0accb24d9771eb91cafdf987
Langston Owens <langstonowens2009@gmail.com>
2026-01-01 23:20:43 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-2413639035-7ed5f",
+  "appId": "1:605207822760:web:cb4bc1e301e93cabeb9d9d",
+  "apiKey": "AIzaSyDrBPt3V4CKoSThhNLHs-XJMh5aWRHiOrg",
+  "authDomain": "studio-2413639035-7ed5f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "605207822760"
 };
